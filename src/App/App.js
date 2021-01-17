d40ba4feb7773b92dca0e8edeb75bde0db03132e
ljw8161 <ljw8161@naver.com>
2021-01-17 15:13:10 +0900

    [UPLOAD] google map api

--- src/App/App.js
+++ src/App/App.js
@@ -2,26 +2,59 @@ import kind from '@enact/core/kind';
 import MoonstoneDecorator from '@enact/moonstone/MoonstoneDecorator';
 import Panels from '@enact/moonstone/Panels';
 import React from 'react';
-
+import { GoogleMap, LoadScript, Marker } from '@react-google-maps/api';
+import Button from '@enact/moonstone/Button';
+import Icon from '@enact/moonstone/Icon';
 import MainPanel from '../views/MainPanel';
-
 import css from './App.module.less';
 
-const App = kind({
-	name: 'App',
 
-	styles: {
-		css,
-		className: 'app'
+const containerStyle = {
+	width: '1600px',
+	height: '900px'
+  };
+  
+  const knucenter = {
+	lat: 12.935504285297199,
+	lng: 77.60565231958368
+  };
+  
+  const icons = {
+	home: {
+	  url: <Icon>home</Icon>,
 	},
-
-	render: (props) => (
-		<div {...props}>
-			<Panels>
-				<MainPanel />
-			</Panels>
+  };
+  
+  function MyComponent() {
+	return (
+		<LoadScript
+		  googleMapsApiKey="AIzaSyBOluu7JbYoawEF0aMk4cgsPk_Fbx5GrGs"
+		>
+		  <GoogleMap
+			mapContainerStyle={containerStyle}
+			center={knucenter}
+			zoom={17}
+		  >
+			{ /* Child components, such as markers, info windows, etc. */ }
+			<Marker
+			  position = {knucenter}
+			  icon = {icons.home}
+			/>
+		  </GoogleMap>
+		</LoadScript>
+		
+	 )
+   }
+  
+  class App extends React.Component {
+	render() {
+	  return (
+		<div>
+		<MyComponent />		
 		</div>
-	)
-});
-
-export default MoonstoneDecorator(App);
+	  );
+	}
+	
+  }
+  
+export default React.memo(App) 
