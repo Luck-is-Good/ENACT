bf7324e85300a2126bcc63af5db848ce305d09e8
ljw8161 <ljw8161@naver.com>
2021-01-18 23:09:05 +0900

    [UPDATE] divide Map

--- src/App/App.js
+++ src/App/App.js
@@ -4,57 +4,12 @@ import kind from '@enact/core/kind';
 import MoonstoneDecorator from '@enact/moonstone/MoonstoneDecorator';
 import Panel from '@enact/moonstone/Panels';
 import React from 'react';
-import { GoogleMap, LoadScript, Marker } from '@react-google-maps/api';
-import Button from '@enact/moonstone/Button';
-import Icon from '@enact/moonstone/Icon';
 import MainPanel from '../views/MainPanel';
 import css from './App.module.less';
 import {Layout, Row, Cell, Column, Item} from '@enact/ui/Layout';
-import IconButton from '@enact/ui/IconButton';
 import {ResolutionDecorator,resolution} from '@enact/ui/resolution';
-import Location from '../views/Location';
 import InputForm from '../views/InputForm';
-
-
-const containerStyle = {
-	width: '100%',
-  	height: '800px'
-}
-
-  
-const knucenter = {
-	lat: 12.935504285297199,
-	lng: 77.60565231958368
-};
-  
-const icons = {
-	home: {
-	  url: <Icon>home</Icon>,
-	},
-};
-  
-function MyComponent() {
-	return (
-		<LoadScript
-		  googleMapsApiKey="AIzaSyBOluu7JbYoawEF0aMk4cgsPk_Fbx5GrGs"
-		>
-		  <GoogleMap
-			mapContainerStyle={containerStyle}
-			center={knucenter}
-			zoom={17}
-		  >
-			{ /* Child components, such as markers, info windows, etc. */ }
-			<Marker
-			  position = {knucenter}
-			  icon = {icons.home}
-			/>
-		  </GoogleMap>
-		</LoadScript>
-		
-	 )
-}
-
-
+import Map from '../components/Map';
 
 const App = kind({
 	name: 'App',
@@ -74,7 +29,7 @@ const App = kind({
 				<Cell>
 				<Row> 
 					<Cell >
-						<MyComponent />
+						<Map />
 					</Cell>
 					<Cell size="30%">
 						<InputForm/>
